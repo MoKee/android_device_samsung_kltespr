3311b4efee8c778dd70191245287535f94b89c98
martincz <martincz@mokeedev.com>
2017-01-19 14:52:22 +0800

    kltespr: mokee-ify

---
+++ mokee.mk
@@ -0,0 +1,6 @@
+$(call inherit-product, vendor/mk/config/common_full_phone.mk)
+
+$(call inherit-product, device/samsung/kltespr/full_kltespr.mk)
+
+PRODUCT_DEVICE := kltespr
+PRODUCT_NAME := mk_kltespr
